75895a05909d3a6a1a869439bfd9495867a79743
xGentuso <ryan.mota@triosstudent.com>
2025-03-18 11:41:14 -0400

    Initial Commit

---
+++ Pong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pong
+//
+//  Created by ryan mota on 2025-03-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pong/PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  PongApp.swift
+//  Pong
+//
+//  Created by ryan mota on 2025-03-18.
+//
+
+import SwiftUI
+
+@main
+struct PongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
